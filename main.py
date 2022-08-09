1c533207835843b373f84cf00677b6ca319982a9
kunal ashok lohar <91951708+kunalashoklohar@users.noreply.github.com>
2022-08-09 09:55:02 +0530

    corrects import statements and debug statements

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-impoprt os
+import os
 import logging
 #add basic config here
 logging.basicConfig(filename="newfile.log",
                     format='%(asctime)s %(message)s',
                     filemode='w',
                     level=logging.NOTSET)
-logging.warning("first warning")                    
+logging.debug("First Debug")                   
